a33ad07bffa9f5f7a1fe1d82041a1ffbb01f7fbf
Sharan Biradar <shade3011@users.noreply.github.com>
2019-07-28 23:14:25 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
+#Team
+#Sharan Basavaraj Biradar - 11915084
+#Aditya Auradker - 11915078
+#Raashi Hasteer - 11915021
+
+#Importing Libraries
 library(udpipe)
 library(ggplot2)
 library(igraph)
@@ -8,21 +14,24 @@ library(tidyr)
 library(tidyverse)
 library(tidytext)
 
+#Loading the english langauge model in case it failed to load from UI.R code execution
 english_model = udpipe_load_model("./english-ewt-ud-2.3-181115.udpipe")
 
+#Below is the code for server processing
 shinyServer(function(input, output) {
   
   input_data <- reactive({
-    
+    #Blank input, don't do anything
     if (is.null(input$file_input)) {
       return(NULL)
     }
     else {
       input_data = readLines(input$file_input$datapath)
-      return(input_data)
+      return(input_data) #Returns the content of the file uploaded
     }
   })
   
+  #This code does annotation of the input data. This only returns a subset of the final result (100 rows). (Slightly time consuming)
   annotate_documents_data <- reactive({
     #annotate_documents_data <- tibble(text = input_data()) %>%
     #  mutate(text = gsub('<.*?>', '', text)) %>% 
@@ -47,6 +56,7 @@ shinyServer(function(input, output) {
                                                       pageLength = 20
                                                     ))
   
+  #This code does annotation of the input data. (Slightly time consuming)
   annotate_documents_data_full <- reactive({
     #annotate_documents_data <- tibble(text = input_data()) %>%
     #  mutate(text = gsub('<.*?>', '', text)) %>% 
@@ -63,6 +73,7 @@ shinyServer(function(input, output) {
     return(annotate_documents_data)
   })
   
+  #Download functionality for the user to download entire annotations data
   output$download_annotated_data <- downloadHandler(
     filename = function(){
       paste('Annotated Data for your Input.csv', sep = '')
@@ -72,6 +83,7 @@ shinyServer(function(input, output) {
     }
   )
   
+  #Creation of separate tabs & populating the word cloud images for both nouns and verbs
   output$wordcloud <- renderUI({
     tabsetPanel(id = "WordCloud Tab", 
                 tabPanel("nouns_wordcloud_tab",
@@ -81,6 +93,7 @@ shinyServer(function(input, output) {
     )
   })
   
+  #Creation of word cloud for nouns based on term frequency
   word_cloud_nouns <- reactive({
     data_for_word_cloud = annotate_documents_data_full()
     all_nouns_in_data = data_for_word_cloud %>% 
@@ -97,6 +110,7 @@ shinyServer(function(input, output) {
   
   output$word_cloud_nouns_tab <- renderPlot({word_cloud_nouns()})
   
+  #Creation of word cloud for verbs based on term frequency
   word_cloud_verbs <- reactive({
     data_for_word_cloud = annotate_documents_data_full()
     all_verbs_in_data = data_for_word_cloud %>% 
@@ -113,6 +127,7 @@ shinyServer(function(input, output) {
   
   output$word_cloud_verbs_tab <- renderPlot({word_cloud_verbs()})
   
+  #Creation of co-occurrence graph for top 30 word combinations
   cooccurrence_graphs_result <- reactive({
     data_for_co_occur = annotate_documents_data_full()
     checkbox_user_input = input$checkbox_group_input_upos
@@ -122,15 +137,15 @@ shinyServer(function(input, output) {
                                           term = "lemma",
                                           group = c("doc_id", "paragraph_id", "sentence_id"))
     
-    co_occur_word_network = head(co_occur_data_computed, 50)
+    co_occur_word_network = head(co_occur_data_computed, 30)
     co_occur_word_network = igraph::graph_from_data_frame(co_occur_word_network)
     
     cooccurrence_graphs_result = ggraph(co_occur_word_network, layout = "fr") +  
       geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "orange") +  
-      geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
-      theme_graph(base_family = "Arial Narrow") +  
+      geom_node_text(aes(label = name), col = "blue", size = 4) +
+      theme_graph(base_family = "Calibri") +  
       theme(legend.position = "none") +
-      labs(title = "Cooccurrences within 3 words distance", subtitle = checkbox_user_input)
+      labs(title = "Cooccurrences of words", subtitle = "For selected POS tags...")
     return(cooccurrence_graphs_result)
   })
   
@@ -140,3 +155,30 @@ shinyServer(function(input, output) {
 }  
 
 )
+
+
+
+
+#Citations
+#1. Class Notes
+#2. https://shiny.rstudio.com/articles/dynamic-ui.html
+#3. https://shiny.rstudio.com/tutorial/written-tutorial/lesson4/
+#4. https://support.rstudio.com/hc/en-us/articles/360007981134-Persistent-Storage-on-RStudio-Connect
+#5. https://help.data.world/hc/en-us/articles/115006300048-GitHub-how-to-find-the-sharable-download-URL-for-files-on-GitHub
+#6. R Shiny Documentation
+#7. https://stackoverflow.com/questions/43155273/how-to-pass-checkbox-input-into-a-function-in-shinyserver
+#8. https://stackoverflow.com/questions/22206290/r-shiny-making-sub-panels
+#9. https://stackoverflow.com/questions/44980227/shiny-server-functions-with-2-outputs
+#10. https://github.com/Lchiffon/wordcloud2/issues/18
+#11. https://stackoverflow.com/questions/35176746/datatable-dt-shiny-r-select-all-found-rows
+#12. https://stackoverflow.com/questions/25126130/r-shiny-renderdatatable-display-options
+#13. https://stackoverflow.com/questions/51730816/remove-showing-1-to-n-of-n-entries-shiny-dt
+#14. https://stackoverflow.com/questions/41464449/shiny-data-frame-only-displays-10-rows
+#15. https://shiny.rstudio.com/reference/shiny/1.0.4/downloadHandler.html
+#16. https://shiny.rstudio.com/reference/shiny/1.0.4/downloadButton.html
+#17. https://shiny.rstudio.com/reference/shiny/0.14/renderDataTable.html
+#18. https://shiny.rstudio.com/reference/shiny/0.14/tableOutput.html
+#19. https://shiny.rstudio.com/articles/download.html
+#20. https://rstudio.github.io/DT/
+#21. https://stackoverflow.com/questions/44504759/shiny-r-download-the-result-of-a-table
+#22. https://stackoverflow.com/questions/41729259/shiny-datatable-save-full-data-frame-with-buttons-extension
--- ui.R
+++ ui.R
@@ -1,3 +1,9 @@
+#Team
+#Sharan Basavaraj Biradar - 11915084
+#Aditya Auradker - 11915078
+#Raashi Hasteer - 11915021
+
+#Importing Libraries
 library(udpipe)
 library(ggplot2)
 library(igraph)
@@ -8,18 +14,20 @@ library(tidyr)
 library(tidyverse)
 library(tidytext)
 
+#Loading the english language model
 english_model = udpipe_load_model("./english-ewt-ud-2.3-181115.udpipe")
 
+#This is the code for Shiny UI below
 shinyUI(fluidPage(
   
-  # Application title
-  titlePanel("UDPipe NLP Workflow"),
+  # Our App title
+  titlePanel("UDPipe NLP Workflow for TABA Assignment"),
   
   sidebarPanel(
-    
+    #This is for the upload button
     fileInput(inputId = "file_input", label = "Upload text file",
               placeholder = "No file selected"),
-    
+    #This is for checkbox to make user choose the POS tags. Noun, Adjective and Proper Nouns are by default
     checkboxGroupInput(inputId = "checkbox_group_input_upos", 
                        label = "Choose the Universal Part of Speech Tags for plotting co-occurrences",
                        choices = c('ADJ', 'NOUN', 'PROPN', 'ADV', 'VERB'),
@@ -32,7 +40,7 @@ shinyUI(fluidPage(
   mainPanel(
     tabsetPanel(
       type = "tabs",
-      
+      #This bit is for the output to be displayed
       tabPanel("Introduction to the App", 
                h4(p("What does this App do?")), 
                p("This App is expected to generate UDPipe NLP pipeline for the input text data.
@@ -41,7 +49,9 @@ shinyUI(fluidPage(
                  generate co-occurrence graph of the top most 30 words."),
                h4(p("What are you expected to do?")), 
                p("Upload a text file that contains sentences. Once uploaded, then click on either of the tabs to get results for the same. 
-                 For the co-occurrence graphs, you are free to check either of the boxes. Happy Adventure!")),
+                 For the co-occurrence graphs, you are free to check either of the boxes. Happy Adventure!"),
+               h4(p("Important Note to User")), 
+               p("Server side processing takes time in proportion to number of sentences in the input text. Hence please be patient! Thanks :D")),
       
       tabPanel("Annotated document",
                h4(p("Annotations for the Input Data")),
@@ -50,7 +60,7 @@ shinyUI(fluidPage(
       
       tabPanel("WordCloud", 
                h4(p("WordCloud Images")),
-               uiOutput("wordcloud")),
+               uiOutput("wordcloud")), #After server processing, this creates two tabs for wordclourds (Nouns & Verbs respectively)
       
       tabPanel("Co-occurrences",
                h4(p("Plotting Co-occurrence Graphs")),
@@ -62,3 +72,27 @@ shinyUI(fluidPage(
   
 ) 
 )
+
+#Citations
+#1. Class Notes
+#2. https://shiny.rstudio.com/articles/dynamic-ui.html
+#3. https://shiny.rstudio.com/tutorial/written-tutorial/lesson4/
+#4. https://support.rstudio.com/hc/en-us/articles/360007981134-Persistent-Storage-on-RStudio-Connect
+#5. https://help.data.world/hc/en-us/articles/115006300048-GitHub-how-to-find-the-sharable-download-URL-for-files-on-GitHub
+#6. R Shiny Documentation
+#7. https://stackoverflow.com/questions/43155273/how-to-pass-checkbox-input-into-a-function-in-shinyserver
+#8. https://stackoverflow.com/questions/22206290/r-shiny-making-sub-panels
+#9. https://stackoverflow.com/questions/44980227/shiny-server-functions-with-2-outputs
+#10. https://github.com/Lchiffon/wordcloud2/issues/18
+#11. https://stackoverflow.com/questions/35176746/datatable-dt-shiny-r-select-all-found-rows
+#12. https://stackoverflow.com/questions/25126130/r-shiny-renderdatatable-display-options
+#13. https://stackoverflow.com/questions/51730816/remove-showing-1-to-n-of-n-entries-shiny-dt
+#14. https://stackoverflow.com/questions/41464449/shiny-data-frame-only-displays-10-rows
+#15. https://shiny.rstudio.com/reference/shiny/1.0.4/downloadHandler.html
+#16. https://shiny.rstudio.com/reference/shiny/1.0.4/downloadButton.html
+#17. https://shiny.rstudio.com/reference/shiny/0.14/renderDataTable.html
+#18. https://shiny.rstudio.com/reference/shiny/0.14/tableOutput.html
+#19. https://shiny.rstudio.com/articles/download.html
+#20. https://rstudio.github.io/DT/
+#21. https://stackoverflow.com/questions/44504759/shiny-r-download-the-result-of-a-table
+#22. https://stackoverflow.com/questions/41729259/shiny-datatable-save-full-data-frame-with-buttons-extension
